85f6d298794fca5ab6683cc467fb030960f94252
Dmitrii Piskov <35172912+Sverring@users.noreply.github.com>
2018-03-09 22:28:18 +0300

    Create scripts.js

---
+++ js-basic/scripts.js
@@ -0,0 +1,6 @@
+let visitor = prompt('What is your nation?');
+let message = 'Hello, ' + visitor + '. One more human in Hell. ';
+message += 'Welcome to the earth\'s Depth, ';
+message += visitor;
+message += '. Please, don\'t forget to clean your boiler!'; 
+document.write(message);
