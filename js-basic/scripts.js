3252c0bb21f6506c25404b34ad63302de83850c9
Dmitrii Piskov <35172912+Sverring@users.noreply.github.com>
2018-03-17 21:53:56 +0300

    Update scripts.js
    Random Generator between 2 numbers.

--- js-basic/scripts.js
+++ js-basic/scripts.js
@@ -1,5 +1,7 @@
-userNumber = prompt('Type a number');
-topNumber = parseInt(userNumber);
-randomNumber = Math.floor(Math.random() * topNumber)+1;
-message = ('<h2>You have got a ' + randomNumber + ' between 1 and ' + topNumber + '.</h2>');
+let userNumber2 = prompt('Type a starting number');
+let bottomNumber = parseInt(userNumber2);
+let userNumber = prompt('Type a number');
+let topNumber = parseInt(userNumber);
+let randomNumber = Math.floor(Math.random() * (topNumber - bottomNumber + 1)) + bottomNumber;
+let message = ('<h2>You have got a ' + randomNumber + ' between ' +bottomNumber + ' and ' + topNumber + '.</h2>');
 document.write(message);
