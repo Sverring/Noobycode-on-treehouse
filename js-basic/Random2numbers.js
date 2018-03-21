8fb6254fe680e837fc7960e726b22a45d5864232
Dmitrii Piskov <35172912+Sverring@users.noreply.github.com>
2018-03-21 18:51:01 +0300

    Update Random2numbers.js

--- js-basic/Random2numbers.js
+++ js-basic/Random2numbers.js
@@ -5,3 +5,17 @@ function randomNumber (bottomNumber, topNumber) {
   return randomMe;
 }
 document.write(randomNumber(15, 75));
+
+
+
+
+//avoiding to type a string like argument: (isNaN - is Not a Number - this method checks the argument (true/false));
+function randomNumber (bottomNumber, topNumber) {
+  if (isNaN(bottomNumber) || isNaN(topNumber)) {
+      throw new Error ('Both arguments must be numbers.');
+  } 
+  return Math.floor(Math.random() * (topNumber - bottomNumber + 1)) + bottomNumber;  
+  }
+console.log(randomNumber(45, 75));
+console.log(randomNumber(34, 'v'));
+
